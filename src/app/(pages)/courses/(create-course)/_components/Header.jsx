2189b14a94ee44b8e44d8dc482806839b3dd1c25
rohitchauhan0 <beatfeelrc121@gmail.com>
2024-10-19 22:08:00 +0530

    tenth commit

--- src/app/(pages)/courses/(create-course)/_components/Header.jsx
+++ src/app/(pages)/courses/(create-course)/_components/Header.jsx
@@ -27,8 +27,6 @@ const Header = () => {
         ) : (
           <UserButton />
         )}
-
-        <ModeToggle />
       </div>
     </div>
   );
--- src/app/dashboard/(admin)/(create-course)/_components/Header.jsx
+++ src/app/dashboard/(admin)/(create-course)/_components/Header.jsx
@@ -28,7 +28,6 @@ const Header = () => {
           <UserButton />
         )}
 
-        <ModeToggle />
       </div>
     </div>
   );
